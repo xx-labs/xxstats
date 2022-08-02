62896cd837ba2c43ca96515aaba81378540c61c8
mariopino <mariopino@protonmail.com>
2022-08-02 12:01:57 +0200

    debug

--- backend/src/lib/staking.ts
+++ backend/src/lib/staking.ts
@@ -288,6 +288,8 @@ export const insertRankingValidator = async (
   startTime: number,
   loggerOptions: LoggerOptions,
 ): Promise<void> => {
+  // debug
+  console.log(JSON.stringify(validator, null, 2));
   const sql = `INSERT INTO ranking (
       block_height,
       rank,
