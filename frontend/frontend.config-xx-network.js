af20d0e00adafa2c23f2b726c6497208e8e8102e
mariopino <mariopino@protonmail.com>
2022-10-13 10:39:55 +0200

    revert

--- frontend/frontend.config-xx-network.js
+++ frontend/frontend.config-xx-network.js
@@ -5,7 +5,7 @@ export const config = {
   tokenDecimals: 9,
   ss58Format: 55,
   coinGeckoDenom: 'xxcoin',
-  nodeWs: 'wss://mainnet.xxnet.io',
+  nodeWs: 'wss://dev.xx-network.polkastats.io/ws',
   backendWs: 'wss://dev.xx-network.polkastats.io/graphql',
   backendHttp: 'https://dev.xx-network.polkastats.io/graphql',
   backendAPI: 'https://dev.xx-network.polkastats.io',
