28dfe644fcab1c7ded77f7776fda99a63a869623
mariopino <mariopino@protonmail.com>
2022-11-01 19:15:16 +0100

    use vue-gtag v1

--- frontend/plugins/gtag.js
+++ frontend/plugins/gtag.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueGtag from 'vue-gtag'
 import { config } from '../frontend.config.js'
 
-Vue.use(VueGtag, {
-  config: { id: config.googleAnalytics },
-})
+if (config.googleAnalytics !== '') {
+  Vue.use(VueGtag, {
+    config: { id: config.googleAnalytics },
+  })
+}
